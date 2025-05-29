af43a00ad7ab58c493e6294b00d8aebfefb65fb7
Hector <hectordiel9@gmail.com>
2025-05-29 13:32:52 +0100

    Merge conflict solved

--- src/HeroSection/Hero.jsx
+++ src/HeroSection/Hero.jsx
@@ -8,11 +8,7 @@ function Hero() {
             <img className="yellow" src="/assets/Hero_YellowPattern.svg" alt="sun" />
             <div className="heroWrapper">
                 <h1>Escolher a si mesma<br />e um ato de <strong>coragem</strong></h1>
-<<<<<<< HEAD
                 <button><h2>vamos conversar?</h2></button>
-=======
-                <button>vamos conversar?</button>
->>>>>>> 65a397f6b6a86a4ba332491836d7a6424e88fc8d
             </div>
             <img className="blue" src="/assets/Hero_bluePattern.svg" alt="Blue pattern" />
         </>
